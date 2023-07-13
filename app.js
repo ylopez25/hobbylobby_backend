ac046057eaf1277c6184915bf0bcaf70ee51893c
Yesenia L <ylopezcr123@gmail.com>
2023-07-13 18:46:59 -0400

    test commit

--- app.js
+++ app.js
@@ -1,54 +1,10 @@
-// //DEPENDENCIES
-// const express = require('express');
-// // const cors = require('cors');
-// const hobbies = require("./hobbies/hobby_db.json");
-
-// //CONFIGURATION
-// const app = express();
-// console.log('hiii, again')
-
-// //MIDDLEWARE
-// // app.use(cors());
-// //app.use(express.json());
-
-// //ROUTES
-// app.get('/', (req,res) => {
-//     //change this to res.send.json
-//     res.send('Welcome to hobby lobby!')
-// })
-
-// //app.use('/hobbies', hobbies)
-// app.get('/hobbies', (request, response) => {
-//     try {
-//       const { students } = hobbies;
-//       response.status(200).json({ data: students });
-//     } catch (err) {
-//       response.status(500).json({ error: err.message });
-//     }
-//   });
-
-// // app.get('/hobbies/:id',(req,res) => {
-// //     try{
-// //         if(hobbies[req.params.id]) {
-// //                 res.send(hobbies[req.params.id])
-// //                }else {
-// //                 res.send('cannot find any hobby at this index')
-// //                }
-// //     }catch(err) {
-// //         res.send('err')
-// //     }
-// //});
-
-// //EXPORT
-// module.exports = app;
-
-
 const express = require('express');
 const userData = require('./usersData.json');
+const cors = require('cors')
 
 
 const app = express();
-
+app.use(cors())
 
 app.get('/', (request, response) => {
 
