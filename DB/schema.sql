9b7263ce9f6a1cd31dc7729aef13501de517e627
Yesenia L <ylopezcr123@gmail.com>
2023-09-27 22:33:06 -0400

    added seed_users and seed_photos

--- DB/schema.sql
+++ DB/schema.sql
@@ -2,19 +2,19 @@ DROP TABLE IF EXISTS photodump;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY ,
-    username varchar(255),
+    id SERIAL PRIMARY KEY,
+    user_name varchar(255),
     city varchar(255),
-    firstName varchar(255),
-    lastName varchar(255),
+    first_name varchar(255),
+    last_name varchar(255),
     pic text,
     skill varchar(255)
 );
 
 CREATE TABLE photodump (
     id SERIAL PRIMARY KEY,
-    userId integer REFERENCES users(id) ON DELETE CASCADE,
+    user_id integer REFERENCES users(id) ON DELETE CASCADE,
     photo text
 );
 
-CREATE INDEX photodump_userId ON photodump(userId)
+CREATE INDEX photodump_user_id ON photodump(user_id)
