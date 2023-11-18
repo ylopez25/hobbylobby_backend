abe2aa73671512bc85003df0edea2fa912f06474
Yesenia L <ylopezcr123@gmail.com>
2023-11-17 22:56:38 -0500

    working post method

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const usersController = require("./controllers/usersController");
 const usersControllerV2 = require("./controllers/usersControllerV2");
+const bodyParser = require('body-parser')
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 app.use("/users", usersController);
 app.use("/v2/users", usersControllerV2);
